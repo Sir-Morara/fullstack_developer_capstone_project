3fd44251126c17b9034eb01e16f3ddd55a248879
Sir-Morara <moraragift@gmail.com>
2024-10-03 13:43:59 +0300

    Modified files for Client Logout Deployment

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -45,7 +45,10 @@ def login_user(request):
     return JsonResponse({"error": "Invalid request method"}, status=400)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
+def logout_user(request):
+    logout(request)
+    data = {"userName":""}
+    return JsonResponse(data)
 # ...
 
 # Create a `registration` view to handle sign up request
