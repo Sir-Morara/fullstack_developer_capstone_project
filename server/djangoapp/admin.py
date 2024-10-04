c4a5b8fdd9dad38789a64461e6a9177d8fb4172b
Sir-Morara <moraragift@gmail.com>
2024-10-04 12:25:14 +0300

    Modified code to deploy CarModel and CarMake models in my Django app

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,29 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1  # Defines how many extra empty car model forms to display
+
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'car_make', 'type', 'year']
+    list_filter = ['car_make', 'type', 'year']
+    search_fields = ['name']
+
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]  # This links CarModel with CarMake in the admin
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
